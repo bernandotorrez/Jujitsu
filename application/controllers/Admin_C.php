69ccf6f1b7b832c6cf37fa8db77ef33ff131d71d
bernandotorrez <bernandotorrez@hotmail.com>
2018-02-13 16:38:18 +0700

    Update Admin_C.php

--- application/controllers/Admin_C.php
+++ application/controllers/Admin_C.php
@@ -5,8 +5,9 @@ class Admin_C extends CI_Controller {
 
   public function __construct(){
     parent::__construct();
-    //$this->load->model('Home_M');
+    $this->load->model('Admin_M');
     $this->load->library('email');
+    $this->load->library('Check_login_admin');
     //$this->cekPendaftaran();
     $this->load->helper('tanggal');
   }
@@ -22,6 +23,25 @@ class Admin_C extends CI_Controller {
     
 
     /*============= END Fungsi untuk memanggil halaman Index =============*/
+  }
+
+  public function approval(){
+    /*============= START Fungsi untuk memanggil halaman Approval =============*/
+
+    $data['title'] = 'Halaman Admin Approval - Jujitsu UPN Veteran Jakarta';
+    //$data['class'] = 'section-white';
+    $data['url'] = $this->uri->uri_string();
+    //$data['total'] = $this->Home_M->jumlah_anggota();
+    $this->template->admin('content_admin/approval',$data);
+    
+
+    /*============= END Fungsi untuk memanggil halaman Approval =============*/
+  }
+
+  public function cari_approval(){
+    $refcode = $this->input->post('refcode');
+
+    $cek = $this->Admin_M->getDataRefCode($refcode);
   }
 
 }
