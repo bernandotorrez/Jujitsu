1f81632ef9052415971f6b50893f5713f4d8cf38
bernandotorrez <bernandotorrez@hotmail.com>
2018-02-13 16:41:53 +0700

    Update Check_login.php (Libraries)
    Update Check_login.php (Libraries)

--- application/libraries/Check_login.php
+++ application/libraries/Check_login.php
@@ -6,7 +6,7 @@ Class Check_login {
 
 	function __construct(){
 		$CI =& get_instance();
-	if($CI->session->userdata('login_member') ){
+	if($CI->session->userdata('login_member') OR $CI->session->userdata('login_admin')){
     redirect(base_url('Home'));
 	}
 	}
